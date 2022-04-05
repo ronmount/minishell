f6611e97c0ff43fa995144a58e84171d6ffff89c
Dasha <delinack@MacBook--ronmount.local>
2022-04-05 21:59:07 +0300

    dlrep

--- src/get_next_line/get_next_line_bonus.c
+++ src/get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "../../include/get_next_line_bonus.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
--- src/get_next_line/get_next_line_utils_bonus.c
+++ src/get_next_line/get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "../../include/get_next_line_bonus.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
